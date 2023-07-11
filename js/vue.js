46d05ba390405ce4fc9fe56ae8ed97e095367088
3-Jhon-0 <133905357+3-Jhon-0@users.noreply.github.com>
2023-07-11 11:20:45 -0300

    Agregado el target="_blank" en el link del carrito

--- js/vue.js
+++ js/vue.js
@@ -15,7 +15,7 @@ const header = Vue.createApp({
                 </div>
                 <div><a href="menu.html">Menu</a></div>
                 <div><a href="contacto.html">Contacto</a></div>
-                <div><a href="../backend/crud/carrito.html">🛒</a></div>
+                <div><a href="../backend/crud/carrito.html" target="_blank">🛒</a></div>
             </aside>`}        
     }
 }).mount("#header")
